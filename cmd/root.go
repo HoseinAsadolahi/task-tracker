3c85f7f5ec33d33dbb803cad25caede8c5547ef2
Hosein <asadolahi.hosein226@gmail.com>
2024-08-22 18:07:25 +0330

    init module And add rootCmd And static Syles

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/HoseinAsadolahi/task-tracker/statics"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "task-cli",
+	Short: "a simple task cli for you",
+	Long: `a simple task cli you can use to manage your daily tasks,
+you can add, delete and update your tasks,
+you can mark them as done or in-progress or leave it as todo,
+you can also see the list of done, todo or in progress tasks.`,
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(statics.ErrorStyle.Render(err.Error()))
+		os.Exit(1)
+	}
+}
---
+++ statics/styles.go
@@ -0,0 +1,26 @@
+package statics
+
+import "github.com/charmbracelet/lipgloss"
+
+var DashStyle = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#DC143C")).
+	Margin(0, 0, 0, 4)
+
+var IdStyle = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#DC143C")).
+	Margin(0, 0, 0, 8)
+
+var ContentStyle = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#FFD700"))
+
+var InfoStyle = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#32CD32"))
+
+var ErrorStyle = lipgloss.NewStyle().
+	Bold(true).
+	Italic(true).
+	Foreground(lipgloss.Color("#FF0000"))
